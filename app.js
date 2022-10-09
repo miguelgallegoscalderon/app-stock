50f2f9ae43c006320eaa19b9cd514184354a4f16
Miguel Gallegos <mhgchugo@gmaile.com>
2022-10-08 22:49:10 -0500

    Cambios

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.get('/',(req, res) =>{
   
     console.log('Petecion recibida')
 
-    res.send('<h1>Hello! con NODEMON</h1>')
+    res.send('<h1>HOLA MUNDO</h1>')
 })
 
 const PORT=process.env.PORT || 3000
  
 app.listen(PORT,()=>{
 
-    console.log('Sevidor escuchando 3000')
+    console.log('Sevidor escuchando ${PORT}')
 })
