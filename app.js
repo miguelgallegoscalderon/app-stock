2064b6b79b95b53e133666afd19b31516165b507
Miguel Gallegos <mhgchugo@gmaile.com>
2022-10-08 22:36:17 -0500

    Tercer commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
 
 app.get('/',(req, res) =>{
@@ -7,7 +9,10 @@ app.get('/',(req, res) =>{
 
     res.send('<h1>Hello! con NODEMON</h1>')
 })
-app.listen(3000,()=>{
+
+const PORT=process.env.PORT || 3000
+ 
+app.listen(PORT,()=>{
 
     console.log('Sevidor escuchando 3000')
 })
